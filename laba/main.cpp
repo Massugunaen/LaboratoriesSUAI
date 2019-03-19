67d0b3dc874fa65bae465b392364f290294a7290
Roman Timashev <roman@tmshv.ru>
2019-03-19 22:41:37 +0300

    Initial Commit

---
+++ laba/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  laba
+//
+//  Created by Roman Timashev on 19/03/2019.
+//  Copyright © 2019 Roman Timashev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
