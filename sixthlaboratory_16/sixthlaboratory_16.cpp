2dcb4ce01063d791cbe3815946c9e61ef37700d2
Alexander Petrosyan <sashapitr@me.com>
2019-05-30 02:52:02 +0300

    1

--- sixthlaboratory_16/sixthlaboratory_16.cpp
+++ sixthlaboratory_16/sixthlaboratory_16.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
 
 int main() {
 
     std::cout << "input mode" << std::endl;
-    std::cout << "supported are: 'auto(a)', 'manual(m)((16_lab))', 'exit(e)'" << std::endl;
+    std::cout << "supported are: 'auto(a)', 'manual(m) /*For16Lab*/', 'exit(e)'" << std::endl;
     char(input_mode_char);
 
     while (std::cin >> input_mode_char) {
@@ -13,64 +13,70 @@ int main() {
             /*this is for training*/
             case 'a': {
                 std::cout <<"input size you want to have" << std::endl;
-                double size_of_a_array;
+                int size_of_a_array(0);
                 std::cin >> size_of_a_array;
                 std::cout << "input the number you want to increase your array" << std::endl;
-                double (d);
+                double d(0);
                 std::cin >> d;
                 std::vector<double> a_array;
+                a_array.reserve(size_of_a_array);
                 /*writing*/
-                for (double i = 0; i <= size_of_a_array; i+=d) {
+                for (double i(0); i < size_of_a_array; i++) {
                     a_array.push_back(i); ///auto-writing
                 }
 
-                /*outputting*/
-                for (double i = 0; i <= size_of_a_array; i+=d) {
+                for (double i(0); i < size_of_a_array; i++) {
                     std::cout << a_array[i] << " ";
                 }
                 std::cout << std::endl;
-
                 break;
             }
 
             /*this is for 16 laboratory*/
             case 'm': {
                 std::cout << "input size of the array" << std::endl;
-                int size_of_m_array, counter(0);
+                int size_of_m_array(0);
                 std::cin >> size_of_m_array; //inputting the size
-                std::vector <int> m_array(size_of_m_array);
-                /*writing numbers*/
-                std::cout << "start writing numbers" << std::endl;
+                std::vector<int> m_array;
+                m_array.reserve(size_of_m_array);
 
-                for (int i = 0; i < size_of_m_array; i++) {
-                    std::cin >> m_array[i];
-                }
+                std::basic_string<char> input_stop_signal;
+                while (input_stop_signal != "stop") {
+                    std::cout << "type anything to start writing numbers or type 'stop' to stop the process"<<std::endl;
+                    std::cin >> input_stop_signal;
 
-                /*filtering*/
-                while (counter < size_of_m_array) {
-                    for (int i = 0; i < size_of_m_array; i++) {
-                        if (m_array[i] == m_array[i+1]) {
-                            for (int j = i; j < size_of_m_array - 1; j++) {
-                                m_array[j] = m_array[j+1];
-                            }
-                            m_array[size_of_m_array - 1] = 0;
-                        }
+                    if (input_stop_signal == "stop") {
+                        return EXIT_SUCCESS;
                     }
-                    counter++;
-                }
 
-                /*outputting them*/
-                std::cout << "The results are the following:" << std::endl;
-                for (int i = 0; i < size_of_m_array; i++) {
-                    std::cout << m_array[i] << " ";
-                }
-                std::cout << std::endl;
+                    else if (input_stop_signal != "stop") {
+                        std::cout << "start writing numbers" << std::endl;
+                        for (int i(0); i < size_of_m_array; i++) {
+                            std::cin >> m_array[i];
+                        }
 
-                break;
+                        for (int k(0); k < size_of_m_array; k++) {
+                            for (int i(0); i < size_of_m_array; i++) {
+                                if (m_array[i] == m_array[i+1]) {
+                                    for (int j(i); j < size_of_m_array; j++) {
+                                        m_array[j] = m_array[j+1];
+                                    }
+                                    m_array[size_of_m_array - 1] = 0;
+                                }
+                            }
+                        }
+
+                        std::cout << "The results are the following:" << std::endl;
+                        for (int i(0); i < size_of_m_array; i++) {
+                            std::cout << m_array[i] << " ";
+                        }
+                        std::cout << std::endl;
+                    }
+                }
             }
 
             case 'e': {
-                return EXIT_SUCCESS;
+                return EXIT_FAILURE;
             }
         }
     }
